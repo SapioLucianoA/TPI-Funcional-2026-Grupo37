19f4333674858be8bce78a73614e57a7ae6a2e58
Sapio Luciano <sapioldev@gmail.com>
2026-06-08 16:59:01 -0300

    funcion transicion antes de comenzar timer

--- Lisp/lisp.lisp
+++ Lisp/lisp.lisp
@@ -1,11 +1,8 @@
-asdasd
-
-(defun clasifico-triangulos (l1 l2 l3)
-	(cond 
-		( (or ( not(numberp l1)) (not(numberp l2)) (not(numberp l3)) )'invalido)
-		( (not (lados-triangulo l1 l2 l3) )'no-es-triangulo)
-			( (= l1 l2 l3 ) 'equilatero) 
-			( (or (= l1 l2) (= l1 l3) (= l2 l3) )'isoceles)
-			(t 'escaleno)
-	)
+(defun  transicion ( color-actual cambiar-a )
+		(cond  
+						((and (eq color-actual  'en-verde) (eq cambiar-a  'amarillo) ) (list color-actual "cambiar-a-amarillo")) 
+						((and (eq color-actual  'en-amarillo)  (eq cambiar-a  'rojo) ) (list color-actual "cambiar-a-rojo"))
+						((and (eq color-actual  'en-rojo ) (eq cambiar-a 'verde) ) (list color-actual "cambiar-a-verde"))
+						(t (list color-actual 'accion-por-defecto))
+		)
 )
